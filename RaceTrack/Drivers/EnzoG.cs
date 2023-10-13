1c56a881d1546b058459ae47ec144f41c6f3d119
breyes4 <71798440+breyes4@users.noreply.github.com>
2023-10-12 19:33:51 -0500

    EnzoG.cs

---
+++ RaceTrack/Drivers/EnzoG.cs
@@ -0,0 +1,28 @@
+﻿using RaceTrack.RaceTrack.Cars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    public class EnzoG : Driver
+    {
+        public EnzoG(RaceCar car) : base(car)
+        {
+            Name = "Enzo Giuseppe";
+            SkillLevel = 4;
+        }
+
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+
+        public override void StopEngine()
+        {
+            Console.WriteLine($"{Name} is driving ver volatile and loses control over the steering wheel and causes engine to run out");
+        }
+    }
+}
