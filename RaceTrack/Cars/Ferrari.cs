b809e63474f7f5e58187031e8311f1273f3abd35
breyes4 <71798440+breyes4@users.noreply.github.com>
2023-10-12 19:29:46 -0500

    Create Ferrari.cs

---
+++ RaceTrack/Cars/Ferrari.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class Ferrari : RaceCar
+    {
+        public Ferrari() 
+        {
+            Name = "Red Ferrari";
+            TopSpeed = 120;
+        }
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} is ready to race!"); ;
+        }
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Name} abrubtly starts slowing down");
+            base.Brake();
+        }
+
+        public override void StopEngine()
+        {
+            Console.WriteLine($"The {Name} blows a tire and is out of the race.");
+        }
+    }
+}
